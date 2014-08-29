6bea205c72093330b830e84eff2aa0fe0476ca78
Tim Woodbury <tim@midpackrunner.com>
2014-08-29 17:55:27 +0000

    Modify config to include personal, first fail/success

--- src/main/java/com/tapadoo/slacknotifier/SlackConfigProcessor.java
+++ src/main/java/com/tapadoo/slacknotifier/SlackConfigProcessor.java
@@ -4,6 +4,7 @@ import jetbrains.buildServer.serverSide.MainConfigProcessor;
 import org.jdom.Attribute;
 import org.jdom.DataConversionException;
 import org.jdom.Element;
+import java.util.HashMap;
 
 /**
  * Created by jasonconnery on 02/03/2014.
@@ -15,45 +16,48 @@ public class SlackConfigProcessor implements MainConfigProcessor {
     public static final String PREF_KEY_SLACK_POSTURL = "slackPostUrl";
     public static final String PREF_KEY_SLACK_LOGOURL = "slackLogoUrl";
 
-    private static final java.lang.String PREF_CHILD_ELEMENT = "slackNotifier";
+    private static final String PREF_CHILD_ELEMENT = "slackNotifier";
 
-    private static final java.lang.String ATTR_NAME_POST_SUCCESSFUL = "postSuccessful" ;
-    private static final java.lang.String ATTR_NAME_POST_STARTED = "postStarted" ;
-    private static final java.lang.String ATTR_NAME_POST_FAILED = "postFailed" ;
-
+    private static final java.lang.String PREF_KEY_NOTIFICATIONS = "notifications";
+    private static final java.lang.String PREF_KEY_NOTIFICATION = "notification";
 
     private String token = "invalidToken";
     private String defaultChannel = "#general";
     private String postUrl;
     private String logoUrl;
+    
+    private HashMap<String, String> notifications = new HashMap();
 
-    private boolean postSuccessful = true ;
-    private boolean postStarted = false ;
-    private boolean postFailed = false ;
-
-    public SlackConfigProcessor()
-    {
+    public SlackConfigProcessor() {
 
     }
 
-    public void init()
-    {
+    public void init() {
 
     }
 
-    public boolean postSuccessful()
-    {
-        return postSuccessful ;
+    public boolean postSuccessful() {
+        return notifications.containsKey('succeeded');
     }
 
-    public boolean postFailed()
-    {
-        return postFailed ;
+    public boolean postFailed() {
+        return notifications.containsKey('failed');
     }
 
-    public boolean postStarted()
-    {
-        return postStarted ;
+    public boolean postStarted() {
+        return notifications.containsKey('started');
+    }
+
+    public boolean postFirstFailure() {
+        return notifications.containsKey('failedAfterSucceeded');
+    }
+
+    public boolean postFirstSuccessful() {
+        return notifications.containsKey('succeededAfterFailed');
+    }
+
+    public boolean postPersonal() {
+        return notifications.containsKey('personal');
     }
 
     public String getToken() {
@@ -103,43 +107,12 @@ public class SlackConfigProcessor implements MainConfigProcessor {
         postUrl = mainConfigElement.getChildText(PREF_KEY_SLACK_POSTURL);
         logoUrl = mainConfigElement.getChildText(PREF_KEY_SLACK_LOGOURL);
 
-        Attribute postSuccessfulAttr = mainConfigElement.getAttribute(ATTR_NAME_POST_SUCCESSFUL);
-        Attribute postStartedAttr = mainConfigElement.getAttribute(ATTR_NAME_POST_STARTED);
-        Attribute postFailedAttr = mainConfigElement.getAttribute(ATTR_NAME_POST_FAILED);
-
-        if( postSuccessfulAttr != null )
-        {
-            try {
-                postSuccessful = postSuccessfulAttr.getBooleanValue();
-            }
-            catch( DataConversionException ex )
-            {
-                postSuccessful = true ;
+        Element notificationsRoot = mainConfigElement.getChild(PREF_KEY_NOTIFICATIONS);
+        if ( notificationsRoot != null ) {
+            for ( Element notification : notificationsRoot.getChildren(PREF_KEY_NOTIFICATION) ) {
+                notifications.put(notification.getTextTrim(), null);
             }
         }
-
-        if( postStartedAttr != null )
-        {
-            try {
-                postStarted = postStartedAttr.getBooleanValue();
-            }
-            catch( DataConversionException ex )
-            {
-                postStarted = false ;
-            }
-        }
-
-        if( postFailedAttr != null )
-        {
-            try {
-                postFailed = postFailedAttr.getBooleanValue();
-            }
-            catch( DataConversionException ex )
-            {
-                postFailed = false ;
-            }
-        }
-
     }
 
     public void writeTo(org.jdom.Element element) {
